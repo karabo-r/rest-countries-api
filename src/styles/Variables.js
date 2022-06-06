9f9fff21852aca357558ac0df2ad41e0f363ca50
Karabo <karaborabothata0@gmail.com>
2022-06-06 19:44:36 +0200

    variables and css reset added

---
+++ src/styles/Variables.js
@@ -0,0 +1,25 @@
+// ## Colors
+// ### Neutral
+const DarkBlueDM = "hsl(209, 23%, 22%)";
+const VeryDarkBlueDM = "hsl(207, 26%, 17%)"
+const VeryDarkBlueLM = "hsl(200, 15%, 8%)"
+const DarkGrayInputLM = "hsl(0, 0%, 52%)"
+const VeryLightGrayBackgroundLM = "hsl(0, 0%, 98%)"
+const White = "hsl(0, 0%, 100%)"
+
+// ## Typography
+// ### Body Copy
+const HomepageItems = "14px"
+const DetailPage = "16px" 
+
+export const Variables = {
+    DarkBlueDM,
+    VeryDarkBlueDM,
+    VeryDarkBlueLM,
+    DarkGrayInputLM,
+    VeryLightGrayBackgroundLM,
+    White,
+    HomepageItems,
+    DetailPage
+}
+
