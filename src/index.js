3fb86ff294da023cff323f7b3e6bd80fcee68077
Karabo <karaborabothata0@gmail.com>
2022-06-16 16:31:27 +0200

    Recoil state added _countryInput

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RecoilRoot } from 'recoil';
 import './styles/StyleReset.css'
 import './styles/index.css'
 import App from './App';
@@ -7,7 +8,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
 );
