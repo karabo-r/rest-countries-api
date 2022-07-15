d6ed845cab360183d70013d39bd549e44c1fcb82
Karabo <karaborabothata0@gmail.com>
2022-07-15 16:46:47 +0200

    restarting project

--- src/App.js
+++ src/App.js
@@ -1,38 +1,5 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
-import CountriesView from './components/CountriesView'
-import Header from './components/Header'
-
-
-
 const App = () => {
+	return <div>App</div>;
+};
 
-  
-  const [isDarkTheme, setIsDarkTheme] = useState(false)
-
-  function changeTheme(){
-    setIsDarkTheme(prev=>!prev)
-    console.log(isDarkTheme);
-  }
-
-  const propsCollection= {
-    changeTheme,
-    isDarkTheme,
-  }
-  
-  return (
-    <Container>
-      <Header {...propsCollection}/>
-      <CountriesView {...propsCollection}/>
-    </Container>
-  )
-}
-
-const Container = styled.div`
-  display: grid;
-  margin: 0;
-  height: 100%;
-  overflow: hidden;
-  grid-template-rows: 8vh 92vh;
-`
-export default App
+export default App;
--- src/recoil/atoms/countryInputAtom.js
+++
@@ -1,6 +0,0 @@
-import {atom} from 'recoil'
-
-export const countrySearchAtom = atom({
-    key: 'countrySearchAtom',
-    default: [],
-});
